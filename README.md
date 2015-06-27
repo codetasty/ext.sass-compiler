3ef13dfaba19612e68f644516b56bebc0e2a474b
animack <ani.mack@gmail.com>
2015-06-27 21:31:43 +0200

    fileManager + styling + underscores

--- README.md
+++ README.md
@@ -7,13 +7,21 @@ sass-compiler is an extension for the code editor CodeTasty that adds automatic 
 
 SASS compile options can be set in the first line of the edited file:
 
-    // out: ../css/style.css
+    // out: ../css/style.css, style: nested
 
 out: compiled file destination
 
     // out: ., app.css, ../style.css
     // . - same path with css extension
     // something.scss - will compile this file instead
+
+style: format output
+
+    // style: nested, expanded, compact, compressed
+
+underscores: prefix imported files with underscores 
+
+    // underscores: true
 
 importing files
 
--- extension.js
+++ extension.js
@@ -6,6 +6,7 @@ define(function(require, exports, module) {
 	var Workspace = require('code/workspace');
 	var Notification = require('code/notification');
 	var Fn = require('code/fn');
+	var FileManager = require('code/fileManager');
 	
 	var Sass = new require('./sass');
 	
@@ -18,6 +19,7 @@ define(function(require, exports, module) {
 		
 	}, {
 		init: function() {
+			var self = this;
 			SassWorker.importer(function(request, done) {
 				var ext = Fn.pathinfo(Extension.importPath).extension;
 				var reqExt = Fn.pathinfo(request.current).extension;
@@ -28,9 +30,15 @@ define(function(require, exports, module) {
 					toLoad += '.' + ext;
 				}
 				
-				toLoad = Extension.parsePath(Extension.importPath, toLoad);
+				if (self._underscores) {
+					toLoad = toLoad.split('/');
+					toLoad[toLoad.length-1] = '_' + toLoad[toLoad.length-1];
+					toLoad = toLoad.join('/');
+				}
 				
-				Extension.getCache(Extension.importWorkspace, toLoad, function(data) {
+				toLoad = FileManager.parsePath(Extension.importPath, toLoad);
+				
+				FileManager.getCache(Extension.importWorkspace, toLoad, function(data) {
 					done({
 						path: toLoad,
 						content: data
@@ -38,207 +46,56 @@ define(function(require, exports, module) {
 				});
 			});
 			
-			EditorSession.on('save', function(data) {
-				// var sess = EditorSession.sessions[data.id];
-				
-				if (data.session.extension == 'sass' || data.session.extension == 'scss') {
-					var cache = Extension.cacheExists(data.session.workspaceId, data.session.path);
-					if (cache) {
-						cache.data = EditorSession.sessions[data.id].lastSavedValue;
-					}
-					
-					Extension.compile(data.session.workspaceId, data.session.path, data.data.getValue());
+			EditorSession.on('save', function(e) {
+				if (self._exts.indexOf(e.storage.extension) !== -1) {
+					Extension.compile(e.storage.workspaceId, e.storage.path, e.session.data.getValue());
 				}
 			});
-			
-			this.checkCache();
 		},
+		_exts: ['scss', 'sass'],
+		_underscores: false,
 		importWorkspace: null,
 		importPath: '',
-		cache: [],
-		getCache: function(workspaceId, path, c) {
-			var found = false;
-			var self = this;
-			
-			this.cache.every(function(cache) {
-				if (cache.workspaceId == workspaceId && cache.path == path) {
-					found = true;
-					
-					cache.lastUsed = new Date().getTime();
-					c(cache.data);
-					
-					return false;
-				}
-				
-				return true;
-			});
-			
-			if (!found) {
-				Socket.send('workspace.action', {
-					action: 'get',
-					id: workspaceId,
-					path: path,
-					forceRemote: true,
-					revision: true,
-				}, false, function(data, stream) {
-					var file = '';
-					if (stream) {
-						stream.on('data', function(chunk) {
-							file += chunk;
-						});
-						
-						stream.on('end', function() {
-							self.addCache(workspaceId, path, file);
-							c(file);
-						});
-					} else {
-						self.addCache(workspaceId, path, file);
-						c(file);
-					}
-				});
-			}
-		},
-		cacheExists: function(workspaceId, path) {
-			var found = false;
-			var self = this;
-			
-			this.cache.every(function(cache) {
-				if (cache.workspaceId == workspaceId && cache.path == path) {
-					found = cache;
-					
-					return false;
-				}
-				
-				return true;
-			});
-			
-			return found;
-		},
-		addCache: function(workspaceId, path, file) {
-			this.cache.push({
-				workspaceId: workspaceId,
-				path: path,
-				data: file,
-				lastUsed: new Date().getTime()
-			});
-		},
-		checkCache: function() {
-			var self = this;
-			
-			var cacheTime = new Date().getTime();
-			cacheTime -= 180*1000;
-			
-			this.cache = this.cache.filter(function (value, index, array) {
-				return (value.lastUsed > cacheTime);
-			});
-			
-			setTimeout(function() {
-				self.checkCache();
-			}, 10000);
-		},
-		getOptions: function(content) {
-			var firstLine = content.substr(0, content.indexOf('\n'));
-			var match = /^\s*\/\/\s*(.+)/.exec(firstLine);
-			var options = {};
-
-			if (!match) {
-				return options;
-			}
-
-			match[1].split(',').forEach(function(item) {
-				var key, value, i = item.indexOf(':');
-				if (i < 0) {
-					return;
-				}
-				key = item.substr(0, i).trim();
-				value = item.substr(i + 1).trim();
-				if (value.match(/^(true|false|undefined|null|[0-9]+)$/)) {
-					value = eval(value);
-				}
-				options[key] = value;
-			});
-			return options;
-		},
-		parsePath: function(path, out, canBeSame) {
-			var destination = out;
-			
-			if (destination == '.' && canBeSame) {
-				destination = path.replace(/(scss|sass)$/, 'css');
-			} else if (out.substr(0, 1) == '/') {
-				destination = out;
-			} else {
-				destination = path.split('/');
-				destination.pop();
-				destination = destination.join('/');
-				destination += '/' + out;
-			}
-			
-			destination = destination.replace(/\/\.\//gi, '/').split('/');
-			
-			destination.every(function(val, key) {
-				if (val == '..') {
-					destination[key] = '';
-					var cc = 1;
-					while ((key-cc) >= 0 && !destination[key-cc]) {
-						cc++;
-					}
-					
-					destination[key-cc] = '';
-				}
-				
-				return true;
-			});
-			
-			destination = destination.join('/').replace(/([\/]+)/gi, '/');
-			
-			if (destination == path) {
-				return false;
-			}
-			
-			return destination;
-		},
 		compile: function(workspaceId, path, doc) {
 			var self = this;
-			var options = this.getOptions(doc);
+			var options = FileManager.getFileOptions(doc);
 			
 			if (!options.out) {
 				return false;
 			}
 			
-			var destination = this.parsePath(path, options.out, true);
+			var destination = FileManager.parsePath(path, options.out, [this._exts.join('|'), 'css']);
 			
 			if (!destination) {
 				return false;
 			}
 			
-			//var mainFile = options.main ? this.parsePath(path, options.main, false) : null;
-			
-			if (!destination.match(/\.(scss|sass)$/)) {
-				this.render(workspaceId, path, doc, options, destination);
-			} else {
-				this.getCache(workspaceId, destination, function(data) {
+			if (destination.match(/\.(scss|sass)$/)) {
+				FileManager.getCache(workspaceId, destination, function(data) {
 					Extension.compile(workspaceId, destination, data);
 				});
+				
+				return false;
 			}
-		},
-		render: function(workspaceId, path, doc, options, destination) {
-			Extension.importWorkspace = workspaceId;
-			Extension.importPath = path;
-			SassWorker.compile(doc, function(result) {
+			
+			this.importWorkspace = workspaceId;
+			this.importPath = path;
+			this._underscores = options.underscores || false;
+			
+			SassWorker.compile(doc, {
+				style: options.style ? Sass.style[options.style] : Sass.style.nested
+			}, function(result) {
 				if (result.status) {
 					Notification.open({
 						type: 'error',
 						title: _('SASS compilation failed.'),
 						description: result.formatted
 					});
-				} else {
-					Socket.send('workspace.action', {
-						id: workspaceId,
-						path: destination,
-						action: 'save',
-						revisions: false
-					}, new Blob([result.text], {type: 'text'}));
+					
+					return false;
 				}
+				
+				FileManager.saveFile(workspaceId, destination, result.text, null);
 			});
 		}
 	});
